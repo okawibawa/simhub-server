e669901e9cf8cf4e1ade4dfc784b8aab0f9dd159
okawibawa <okaa.wibawa@gmail.com>
2024-06-15 16:01:11 +0800

    refactor: env validation code and package.json scripts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,22 +1,44 @@
+import path from "path";
+import fs from "fs";
 import { z } from "zod";
 import { config } from "dotenv";
 
 config();
 
 const environmentSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]),
   PORT: z.coerce.number().int(),
   DATABASE_URL: z.string(),
 });
 
-const parsedEnvironment = environmentSchema.safeParse(process.env);
-
-if (!parsedEnvironment.success) {
-  console.error("Invalid environment variables:", parsedEnvironment.error.format());
-  process.exit(1);
-}
-
-export const environment = {
-  port: parsedEnvironment.data.PORT,
-  databaseUrl: parsedEnvironment.data.DATABASE_URL,
+const getEnvironmentPath = () => {
+  return path.resolve(new URL(import.meta.url).pathname, `../../../.env.${process.env.NODE_ENV}`);
 };
+
+const loadEnvironmentVariables = () => {
+  const environmentPath = getEnvironmentPath();
+
+  if (!fs.existsSync(environmentPath)) {
+    console.warn(`Environment file for ${process.env.NODE_ENV} is not found.`);
+    return;
+  }
+
+  config({ path: environmentPath });
+};
+
+const validateEnvironment = () => {
+  loadEnvironmentVariables();
+
+  const parsedEnvironment = environmentSchema.safeParse(process.env);
+
+  if (!parsedEnvironment.success) {
+    console.error("Invalid environment variables:", parsedEnvironment.error.format());
+    process.exit(1);
+  }
+
+  return {
+    port: parsedEnvironment.data.PORT,
+    databaseUrl: parsedEnvironment.data.DATABASE_URL,
+  };
+};
+
+export const environment = validateEnvironment();
